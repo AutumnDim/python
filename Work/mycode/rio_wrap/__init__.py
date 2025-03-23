b72f8fd95827ee2592fa36190f5c0c27252fa4f1
802 <168349932+AutumnDim@users.noreply.github.com>
2025-03-23 22:16:02 +0800

    Add files via upload

---
+++ Work/mycode/rio_wrap/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 18 10:07:56 2024
+
+@author: wly
+"""
+from mycode.rio_wrap.core.api import (clip,
+                                      reproject,
+                                      resampling,
+                                      unify)
+
+from .warnings import SetNodataWarning,SetRasterAttrWarning
+
+
+'''待修reproject, nodata=np.nan'''
+
+
+
+
+
+
+
+
+
+
+
