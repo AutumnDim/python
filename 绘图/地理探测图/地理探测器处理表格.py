d5f2a3bdacfad112d5339a78eac0db6df6af30b5
802 <168349932+AutumnDim@users.noreply.github.com>
2025-04-07 19:03:17 +0800

    Update 地理探测器处理表格.py

--- 绘图/地理探测图/地理探测器处理表格.py
+++ 绘图/地理探测图/地理探测器处理表格.py
@@ -4,13 +4,19 @@ Created on Thu Nov  7 22:07:15 2024
 
 @author: hqm
 """
+
 import pandas as pd
 import numpy as np 
-path = r"F:\学长数据\元彬学长\our\表\intk.csv"
+path = r"F:\学长数据\元彬学长\hu\hu\int.csv"
+path2 = r"F:\学长数据\元彬学长\hu\hu\p_q.csv"
+data2 = pd.read_csv(path2)
+Q = data2.drop(index =1).T
 k = pd.read_csv(path)
 df = pd.DataFrame(k)
-df = df.iloc[:,1:]
-df = df.pivot(index='var1', columns='var2', values='qv12')
+dff = df.iloc[:,1:]
+df = dff.pivot(index='var1', columns='var2', values='qv12')
+dfk = dff.pivot(index='var1', columns='var2', values='interaction')
 index = df.isna().sum(axis = 1).sort_values(ascending=True)
 columns = df.isna().sum(axis = 0).sort_values(ascending=False)
 sorted_df = df.loc[index.index, columns.index]
+eff_df = dfk.loc[index.index, columns.index]
